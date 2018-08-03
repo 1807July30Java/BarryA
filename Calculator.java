3daefe4a01c8bc62be5e6d22b2b48445f3f00b99
AlphaBarr <41920229+AlphaBarr@users.noreply.github.com>
2018-08-03 12:11:09 -0400

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,11 +4,9 @@ public class Calculator {
 	public String add (String a, String b, String dataType)  {
 		String result = null;
 		
-		if (dataType.equalsIgnoreCase("double")) {
-			
+		if (dataType.equalsIgnoreCase("double")) {            //Double
 			double three = 0;
 			String ans = "Please enter a valid number";
-			
 				
 				try {
 					double one = Double.parseDouble(a);
@@ -24,7 +22,7 @@ public class Calculator {
 			
 				result = Double.toString(three);
 				
-		}else if (dataType.equalsIgnoreCase("int")) {
+		}else if (dataType.equalsIgnoreCase("int")) {       //Int
 			int three = 0;
 			String ans = "Please enter a valid number";
 			
@@ -62,23 +60,42 @@ public class Calculator {
 				result = Long.toString(three);
 		}else if (dataType.equalsIgnoreCase("short")) {
 			short three = 0;
-			String ans = "Please enter a valid number";
-			
-				
+			String ans = "Please enter a valid number";	
 				try {
 					short one = Short.parseShort(a);
 					short two = Short.parseShort(b);
 					three = (short) (one + two);
+				} catch (NumberFormatException e) {
+					System.out.println(ans+ " your operation can not be completed");
+				} 	
+				result = Short.toString(three);	
+		}else if (dataType.equalsIgnoreCase("byte")) {
+			byte three = 0;
+			String ans = "Please enter a valid number";	
+				try {
+					byte one = Byte.parseByte(a);
+					byte two = Byte.parseByte(b);
+					three = (byte) (one + two);
 					
 				} catch (NumberFormatException e) {
-					// TODO Auto-generated catch block
-					
 					System.out.println(ans+ " your operation can not be completed");
+				} 	
+				result = Short.toString(three);	
+			
+		}else if (dataType.equalsIgnoreCase("float")) {
+			
+			float three = 0;
+			String ans = "Please enter a valid number";	
+				try {
+					float one = Float.parseFloat(a);
+					float two = Float.parseFloat(b);
+					three = (one + two);
 					
-				} 
-				
-				result = Short.toString(three);
-				
+				} catch (NumberFormatException e) {
+					System.out.println(ans+ " your operation can not be completed");
+				} 	
+				result = Float.toString(three);	
+			
 		}
 			
 		
@@ -88,5 +105,327 @@ public class Calculator {
 		
 		
 	}
+	
+	//Multiply 
+	
+	public String multiply (String a, String b, String dataType)  {
+		String result = null;
+		
+		if (dataType.equalsIgnoreCase("double")) {            //Double
+			double three = 0;
+			String ans = "Please enter a valid number";
+				
+				try {
+					double one = Double.parseDouble(a);
+					double two = Double.parseDouble(b);
+					three = one * two;
+					
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					
+					System.out.println(ans+ " your operation can not be completed");
+					
+				} 		
+			
+				result = Double.toString(three);
+				
+		}else if (dataType.equalsIgnoreCase("int")) {       //Int
+			int three = 0;
+			String ans = "Please enter a valid number";
+			
+				
+				try {
+					int one = Integer.parseInt(a);
+					int two = Integer.parseInt(b);
+					three = one * two;
+					
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					
+					System.out.println(ans+ " your operation can not be completed");
+					
+				} 
+			
+				result = Integer.toString(three);
+		}else if (dataType.equalsIgnoreCase("long")) {
+			long three = 0;
+			String ans = "Please enter a valid number";
+			
+				
+				try {
+					long one = Long.parseLong(a);
+					long two = Long.parseLong(b);
+					three = one * two;
+					
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					
+					System.out.println(ans+ " your operation can not be completed");
+					
+				} 
+			
+				result = Long.toString(three);
+		}else if (dataType.equalsIgnoreCase("short")) {
+			short three = 0;
+			String ans = "Please enter a valid number";	
+				try {
+					short one = Short.parseShort(a);
+					short two = Short.parseShort(b);
+					three = (short) (one * two);
+				} catch (NumberFormatException e) {
+					System.out.println(ans+ " your operation can not be completed");
+				} 	
+				result = Short.toString(three);	
+		}else if (dataType.equalsIgnoreCase("byte")) {
+			byte three = 0;
+			String ans = "Please enter a valid number";	
+				try {
+					byte one = Byte.parseByte(a);
+					byte two = Byte.parseByte(b);
+					three = (byte) (one * two);
+					
+				} catch (NumberFormatException e) {
+					System.out.println(ans+ " your operation can not be completed");
+				} 	
+				result = Short.toString(three);	
+			
+		}else if (dataType.equalsIgnoreCase("float")) {
+			
+			float three = 0;
+			String ans = "Please enter a valid number";	
+				try {
+					float one = Float.parseFloat(a);
+					float two = Float.parseFloat(b);
+					three = (one * two);
+					
+				} catch (NumberFormatException e) {
+					System.out.println(ans+ " your operation can not be completed");
+				} 	
+				result = Float.toString(three);	
+			
+		}
+			
+		
+		return result;
+	
+			
+		
+		
+	}
+	
+// Substract
+	
+	public String substract (String a, String b, String dataType)  {
+		String result = null;
+		
+		if (dataType.equalsIgnoreCase("double")) {            //Double
+			double three = 0;
+			String ans = "Please enter a valid number";
+				
+				try {
+					double one = Double.parseDouble(a);
+					double two = Double.parseDouble(b);
+					three = one - two;
+					
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					
+					System.out.println(ans+ " your operation can not be completed");
+					
+				} 		
+			
+				result = Double.toString(three);
+				
+		}else if (dataType.equalsIgnoreCase("int")) {       //Int
+			int three = 0;
+			String ans = "Please enter a valid number";
+			
+				
+				try {
+					int one = Integer.parseInt(a);
+					int two = Integer.parseInt(b);
+					three = one - two;
+					
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					
+					System.out.println(ans+ " your operation can not be completed");
+					
+				} 
+			
+				result = Integer.toString(three);
+		}else if (dataType.equalsIgnoreCase("long")) {
+			long three = 0;
+			String ans = "Please enter a valid number";
+			
+				
+				try {
+					long one = Long.parseLong(a);
+					long two = Long.parseLong(b);
+					three = one - two;
+					
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					
+					System.out.println(ans+ " your operation can not be completed");
+					
+				} 
+			
+				result = Long.toString(three);
+		}else if (dataType.equalsIgnoreCase("short")) {
+			short three = 0;
+			String ans = "Please enter a valid number";	
+				try {
+					short one = Short.parseShort(a);
+					short two = Short.parseShort(b);
+					three = (short) (one - two);
+				} catch (NumberFormatException e) {
+					System.out.println(ans+ " your operation can not be completed");
+				} 	
+				result = Short.toString(three);	
+		}else if (dataType.equalsIgnoreCase("byte")) {
+			byte three = 0;
+			String ans = "Please enter a valid number";	
+				try {
+					byte one = Byte.parseByte(a);
+					byte two = Byte.parseByte(b);
+					three = (byte) (one - two);
+					
+				} catch (NumberFormatException e) {
+					System.out.println(ans+ " your operation can not be completed");
+				} 	
+				result = Short.toString(three);	
+			
+		}else if (dataType.equalsIgnoreCase("float")) {
+			
+			float three = 0;
+			String ans = "Please enter a valid number";	
+				try {
+					float one = Float.parseFloat(a);
+					float two = Float.parseFloat(b);
+					three = (one - two);
+					
+				} catch (NumberFormatException e) {
+					System.out.println(ans+ " your operation can not be completed");
+				} 	
+				result = Float.toString(three);	
+			
+		}
+			
+		
+		return result;
+			
+	}
+	
+	// Divide
+	
+	public String divide (String a, String b, String dataType)  {
+		String result = null;
+		
+		if (dataType.equalsIgnoreCase("double")) {            //Double
+			double three = 0;
+			String ans = "Please enter a valid number";
+				
+				try {
+					double one = Double.parseDouble(a);
+					double two = Double.parseDouble(b);
+					three = one / two;
+					
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					
+					System.out.println(ans+ " your operation can not be completed");
+					
+				} 		
+			
+				result = Double.toString(three);
+				
+		}else if (dataType.equalsIgnoreCase("int")) {       //Int
+			int three = 0;
+			String ans = "Please enter a valid number";
+			
+				
+				try {
+					int one = Integer.parseInt(a);
+					int two = Integer.parseInt(b);
+					three = one / two;
+					
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					
+					System.out.println(ans+ " your operation can not be completed");
+					
+				} 
+			
+				result = Integer.toString(three);
+		}else if (dataType.equalsIgnoreCase("long")) {
+			long three = 0;
+			String ans = "Please enter a valid number";
+			
+				
+				try {
+					long one = Long.parseLong(a);
+					long two = Long.parseLong(b);
+					three = one / two;
+					
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					
+					System.out.println(ans+ " your operation can not be completed");
+					
+				} 
+			
+				result = Long.toString(three);
+		}else if (dataType.equalsIgnoreCase("short")) {
+			short three = 0;
+			String ans = "Please enter a valid number";	
+				try {
+					short one = Short.parseShort(a);
+					short two = Short.parseShort(b);
+					three = (short) (one / two);
+				} catch (NumberFormatException e) {
+					System.out.println(ans+ " your operation can not be completed");
+				} 	
+				result = Short.toString(three);	
+		}else if (dataType.equalsIgnoreCase("byte")) {
+			byte three = 0;
+			String ans = "Please enter a valid number";	
+				try {
+					byte one = Byte.parseByte(a);
+					byte two = Byte.parseByte(b);
+					three = (byte) (one / two);
+					
+				} catch (NumberFormatException e) {
+					System.out.println(ans+ " your operation can not be completed");
+				} 	
+				result = Short.toString(three);	
+			
+		}else if (dataType.equalsIgnoreCase("float")) {
+			
+			float three = 0;
+			String ans = "Please enter a valid number";	
+				try {
+					float one = Float.parseFloat(a);
+					float two = Float.parseFloat(b);
+					three = (one / two);
+					
+				} catch (NumberFormatException e) {
+					System.out.println(ans+ " your operation can not be completed");
+				} 	
+				result = Float.toString(three);	
+			
+		}
+			
+		
+		return result;
+	
+			
+		
+		
+	}
+	
+	
+	
+	
 
 }
