77b0d6879249c86d325fe1d5a4d32e4ce7ab0981
AlphaBarr <41920229+AlphaBarr@users.noreply.github.com>
2018-08-17 19:01:39 -0400

    Project1/

--- Project1/src/main/java/com/revature/pojo/Employee.java
+++ Project1/src/main/java/com/revature/pojo/Employee.java
@@ -1,5 +1,93 @@
 package com.revature.pojo;
 
 public class Employee {
+	
+	private double empID;
+	private String empFirstName;
+	private String empLastName;
+	private String empEmail;
+	private String empPassword;
+	private double managerID;
+	
+/************************************************************************/
+//Constructors	
+	
+	public Employee(String empFirstName, String empLastName, String empEmail, String empPassword, double managerID) {
+		super();
+		this.empFirstName = empFirstName;
+		this.empLastName = empLastName;
+		this.empEmail = empEmail;
+		this.empPassword = empPassword;
+		this.managerID = managerID;
+}
+
+	public Employee(double empID, String empFirstName, String empLastName, String empEmail, String empPassword,
+		double managerID) {
+		super();
+		this.empID = empID;
+		this.empFirstName = empFirstName;
+		this.empLastName = empLastName;
+		this.empEmail = empEmail;
+		this.empPassword = empPassword;
+		this.managerID = managerID;
+}
+
+	public Employee() {
+		super();
+	}
+	
+	
+	
+
+/************************************************************************/
+	//Getters and Setters
+	public double getEmpID() {
+		return empID;
+	}
+	public void setEmpID(double empID) {
+		this.empID = empID;
+	}
+	public String getEmpFirstName() {
+		return empFirstName;
+	}
+	public void setEmpFirstName(String empFirstName) {
+		this.empFirstName = empFirstName;
+	}
+	public String getEmpLastName() {
+		return empLastName;
+	}
+	public void setEmpLastName(String empLastName) {
+		this.empLastName = empLastName;
+	}
+	public String getEmpEmail() {
+		return empEmail;
+	}
+	public void setEmpEmail(String empEmail) {
+		this.empEmail = empEmail;
+	}
+	public String getEmpPassword() {
+		return empPassword;
+	}
+	public void setEmpPassword(String empPassword) {
+		this.empPassword = empPassword;
+	}
+	public double getManagerID() {
+		return managerID;
+	}
+	public void setManagerID(double managerID) {
+		this.managerID = managerID;
+	}
+/************************************************************************/
+	//To string
+	@Override
+	public String toString() {
+		return "Employee [empID=" + empID + ", empFirstName=" + empFirstName + ", empLastName=" + empLastName
+			+ ", empEmail=" + empEmail + ", empPassword=" + empPassword + ", managerID=" + managerID + "]";
+	}
+
+
+
+
+
 
 }
--- Project1/src/main/java/com/revature/pojo/Reimbursment.java
+++ Project1/src/main/java/com/revature/pojo/Reimbursment.java
@@ -1,5 +1,106 @@
 package com.revature.pojo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.omg.CosNaming.BindingIterator;
+
 public class Reimbursment {
+	private double reimbID;
+	private String reimbReason;
+	private byte[] reimbEvidence;
+	private Date reimbProcessed;
+	private String reimbStatus;
+	private Date reimbClosed;
+/**********************************************************************************************************************************************************/
+	//Constructors
+	
+public Reimbursment(String reimbReason, byte[] reimbEvidence) {
+	super();
+	this.reimbReason = reimbReason;
+	this.reimbEvidence = reimbEvidence;
+	this.reimbProcessed = new Date();
+}
+	
+	
+public Reimbursment(double reimbID, String reimbReason, byte[] reimbEvidence, String reimbProcessed, String reimbStatus,
+		String reimbClosed) {
+	super();
+	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	this.reimbID = reimbID;
+	this.reimbReason = reimbReason;
+	this.reimbEvidence = reimbEvidence;
+	this.reimbStatus = reimbStatus;
+	try {
+		this.reimbProcessed = formatter.parse(reimbProcessed);
+		this.reimbClosed = formatter.parse(reimbClosed);
+	} catch (ParseException e) {
+		System.out.println("Enter Valid Date");
+	}
+	
+	
+}
+
+public Reimbursment(double reimbID, String reimbReason, byte[] reimbEvidence, String reimbProcessed, String reimbStatus) {
+	super();
+	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	this.reimbID = reimbID;
+	this.reimbReason = reimbReason;
+	this.reimbEvidence = reimbEvidence;
+	this.reimbStatus = reimbStatus;
+	try {
+		this.reimbProcessed = formatter.parse(reimbProcessed);
+	} catch (ParseException e) {
+		System.out.println("Enter Valid Date");
+	}
+	
+	
+}
+
+
+
+/**********************************************************************************************************************************************************/
+	//Getters and Setters
+	public double getReimbID() {
+		return reimbID;
+	}
+
+	public void setReimbID(double reimbID) {
+		this.reimbID = reimbID;
+	}
+	public String getReimbReason() {
+		return reimbReason;
+	}
+	public void setReimbReason(String reimbReason) {
+		this.reimbReason = reimbReason;
+	}
+	public byte[] getReimbEvidence() {
+		return reimbEvidence;
+	}
+	public void setReimbEvidence(byte[] reimbEvidence) {
+		this.reimbEvidence = reimbEvidence;
+	}
+	public Date getReimbProcessed() {
+		return reimbProcessed;
+	}
+	public void setReimbProcessed(Date reimbProcessed) {
+		this.reimbProcessed = reimbProcessed;
+	}
+	public String getReimbStatus() {
+		return reimbStatus;
+	}
+	public void setReimbStatus(String reimbStatus) {
+		this.reimbStatus = reimbStatus;
+	}
+	public Date getReimbClosed() {
+		return reimbClosed;
+	}
+	public void setReimbClosed(Date reimbClosed) {
+		this.reimbClosed = reimbClosed;
+	}
+	
+	/**********************************************************************************************************************************************************/
+	
 
 }
